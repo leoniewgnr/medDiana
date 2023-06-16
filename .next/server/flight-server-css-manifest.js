4c3cfc5c4ddb8e883d6afcbd41d36d6971805abe
leoniewgnr <wagner.leonie@tum.de>
2023-06-16 15:44:50 -0700

    initial

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/leoniewagner/Documents/Coding/medDiana/app/layout.tsx": [
+      "/Users/leoniewagner/Documents/Coding/medDiana/node_modules/.pnpm/next@13.4.3_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/leoniewagner/Documents/Coding/medDiana/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/leoniewagner/Documents/Coding/medDiana/app/page": [
+      "/Users/leoniewagner/Documents/Coding/medDiana/app/globals.css",
+      "/Users/leoniewagner/Documents/Coding/medDiana/node_modules/.pnpm/next@13.4.3_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
